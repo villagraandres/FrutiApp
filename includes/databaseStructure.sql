fc1718b04f9214a22a02ee20b3523b7ae2fc1093
Andrés Nehuel <villagraandres00@gmail.com>
2022-07-13 11:04:07 -0500

    Reestableciendo estructura de DB

--- includes/databaseStructure.sql
+++ includes/databaseStructure.sql
@@ -38,9 +38,7 @@ CREATE TABLE `ordenes` (
 -- Volcado de datos para la tabla `ordenes`
 --
 
-INSERT INTO `ordenes` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
-(111, '2022-07-28', '18:03:00', 42),
-(112, '2022-07-20', '18:03:00', 42);
+
 
 -- --------------------------------------------------------
 
@@ -59,11 +57,6 @@ CREATE TABLE `ordenesproductos` (
 -- Volcado de datos para la tabla `ordenesproductos`
 --
 
-INSERT INTO `ordenesproductos` (`id`, `ordenesId`, `productoId`, `cantidad`) VALUES
-(105, 111, 62, 2),
-(106, 111, 63, 1),
-(107, 112, 63, 2),
-(108, 112, 64, 1);
 
 -- --------------------------------------------------------
 
@@ -84,12 +77,6 @@ CREATE TABLE `productos` (
 --
 -- Volcado de datos para la tabla `productos`
 --
-
-INSERT INTO `productos` (`id`, `nombre`, `resumen`, `descripcion`, `precio`, `imagen`, `seccion`) VALUES
-(61, 'Prueba larga ', 'adasda', 'orem ipsum dolor sit amet, consectetur adipiscing elit. Proin magna ipsum, commodo in leo nec, molestie convallis nisi. Vivamus sed ante quis dui dignissim lacinia at sit amet felis. Pellentesque vulputate elit tortor. Sed ac lacus nec dolor euismod porta eu non orci. In efficitur a nisi eget pulvinar. Cras rutrum ligula sed odio pulvinar, nec suscipit eros cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus.\r\n\r\nNulla quis commodo lorem. In rhoncus, elit eget congue sollicitudin, justo sapien mollis eros, at ornare tellus est quis justo. Donec nec eros vel quam feugiat faucibus. Aliquam lacus sapien, sagittis at urna at, efficitur luctus ipsum. Morbi dignissim mi enim, non imperdiet mi tempus elementum. Morbi faucibus a ex et dapibus. Fusce placerat varius ligula, nec feugiat ligula mollis vitae. Cras vel tellus vel lacus vulputate tincidunt vel ac purus. Vestibulum at leo odio. Praesent interdum, nulla sed aliquam iaculis, dolor lorem semper massa, tempor egestas sem elit sit amet tortor. Cras egestas tellus orci, vel sollicitudin justo consectetur sed. Etiam lacinia massa at eleifend accumsan.', '121212.00', 'bf4445380769a29ab1ca2cb22fe1d28b.jpg', 1),
-(62, 'dasd', 'asda', 'dasda', '2.00', 'fd88032bafa8181d3de143348e5eaaac.jpg', 1),
-(63, 'dasd', 'a', 'd', '2.00', 'd39ce85ac7caf3424307db3cfd7cfcee.jpg', 1),
-(64, 'Pedroa', 'a', 'da', '2.00', '7bc7e32ac05b727324ef930b8dabe67b.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -113,10 +100,6 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `telefono`, `rol`, `token`, `password`, `confirmado`) VALUES
-(39, '', '', 'correo@correo.com', '', 1, '', '$2y$10$UlAIf40w9RfDR9/L70PAkudaaPjUu8GF4E/bCMZvDL76CQ15neYra', 1),
-(42, 'Andrés', 'Villagra', 'villagraandres00@gmail.com', '8125976580', 0, '', '$2y$10$Ef5Im1oUrbZiefzqTHQFMeEow9BFylCYaUtdi9zrUIGmEQB/2pC6m', 1),
-(43, 'Pedro', 'Villagra', 'cuentadobleverificacion@gmail.com', '8125976580', 0, '', '$2y$10$gghdXfOjrgZjup7L6HCxZuCBb8/AJ20ats3G1HYgi.sfteMkixD3u', 1);
 
 --
 -- Índices para tablas volcadas
@@ -157,25 +140,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `ordenes`
 --
 ALTER TABLE `ordenes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `ordenesproductos`
 --
 ALTER TABLE `ordenesproductos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Restricciones para tablas volcadas
